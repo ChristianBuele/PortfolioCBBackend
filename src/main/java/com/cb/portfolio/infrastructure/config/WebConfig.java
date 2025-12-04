909c41ceec0f45048aaa1555878b5a361f42ae03
christian.buele <christian.buele@ucuenca.edu.ec>
2025-12-04 16:50:30 -0500

    Fix: cors

---
+++ src/main/java/com/cb/portfolio/infrastructure/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.cb.portfolio.infrastructure.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // todas las rutas
+                        .allowedOrigins(
+                                "http://localhost:5173"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(true) // si usas cookies / Authorization con credenciales
+                        .maxAge(3600);
+            }
+        };
+    }
+}
